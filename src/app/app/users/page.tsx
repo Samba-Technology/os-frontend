1a448a0ded79b6a37eb4b6f71318304a051d5a4b
Frooszy <thecryzin@gmail.com>
2024-03-04 21:03:20 -0300

    Users DataGrid added

--- src/app/app/users/page.tsx
+++ src/app/app/users/page.tsx
@@ -3,13 +3,48 @@ import StudentsDialog from "@/components/users/studentsDialog";
 import UsersDialog from "@/components/users/usersDialog";
 import AuthContext from "@/contexts/auth";
 import { isAdmin } from "@/helpers/authorization";
-import { Box, Button, Container, CssBaseline } from "@mui/material";
+import { User } from "@/models/user.model";
+import { Box, Button, Container, CssBaseline, IconButton, Paper, TextField, Typography } from "@mui/material";
+import { DataGrid, GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import { UsersService } from "@/services/api/users.service";
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+
+const columns: GridColDef[] = [
+    {
+        field: 'name',
+        headerName: 'Nome',
+        flex: 1
+    },
+    {
+        field: 'email',
+        headerName: 'Email',
+        width: 230
+    },
+    {
+        field: 'actions',
+        type: 'actions',
+        sortable: false,
+        getActions: (params) => [
+            <GridActionsCellItem icon={<ManageAccountsIcon />} onClick={() => console.log(params)} label="Editar Usuário" />,
+            <GridActionsCellItem icon={<DeleteIcon />} onClick={() => console.log(params)} label="Deletar Usuário" />
+        ]
+    }
+]
 
 export default function Login() {
     const [open, setOpen] = useState(false)
     const [openStudents, setOpenStudents] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState<User[]>([])
+    const [pagination, setPagination] = useState({
+        page: 0,
+        pageSize: 10
+    })
+    const [total, setTotal] = useState(0)
 
     const { user } = useContext(AuthContext)
     const router = useRouter()
@@ -20,6 +55,24 @@ export default function Login() {
         }
     }, [])
 
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoading(true)
+                const users = await UsersService.findUsers(pagination.page + 1, pagination.pageSize)
+                setUsers(users.data)
+                setTotal(users.data.total)
+            } catch (e) {
+                console.error(e)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchUsers()
+    }, [pagination])
+
+
     const handleClose = () => {
         setOpen(false)
         setOpenStudents(false)
@@ -27,13 +80,29 @@ export default function Login() {
 
     return (
         <Box className="flex justify-center items-center min-h-screen flex-col">
-            <Container component="main" maxWidth="xs">
+            <Container component="main" maxWidth="md">
                 <CssBaseline />
-                <Button variant="contained" onClick={() => setOpen(true)}>Criar Usuário</Button>
-                <Button variant="contained" onClick={() => setOpenStudents(true)}>Criar Estudante</Button> 
-                <UsersDialog isOpen={open} onClose={handleClose} />
-                <StudentsDialog isOpen={openStudents} onClose={handleClose} />
+                <Paper elevation={3} className="flex flex-col gap-2 p-6">
+                    <Typography variant="h4">Usuários</Typography>
+                    <Box component="div" className="flex flex-col gap-4 mt-2">
+                        <Box component="div" className="flex gap-2 items-center">
+                            <TextField type="search" placeholder="Procurar" fullWidth />
+                            <IconButton onClick={() => setOpen(true)} size="large"><PersonAddAlt1Icon /></IconButton>
+                        </Box>
+                        <DataGrid
+                            rows={users}
+                            loading={loading}
+                            columns={columns}
+                            paginationMode="server"
+                            pageSizeOptions={[10, 20, 30, 40]}
+                            paginationModel={pagination}
+                            onPaginationModelChange={setPagination}
+                            rowCount={total}
+                        />
+                    </Box>
+                </Paper>
             </Container>
+            <UsersDialog isOpen={open} onClose={handleClose} />
         </Box>
     )
 
--- src/services/api/users.service.ts
+++ src/services/api/users.service.ts
@@ -12,4 +12,13 @@ export class UsersService {
     public static findMe() {
         return api.get('/users/me').then(response => response.data)
     }
+
+    public static findUsers(page: number, limit: number) {
+        return api.get('/users', {
+            params: {
+                page: page,
+                limit: limit
+            }
+        }).then(response => response.data)
+    }
 }
