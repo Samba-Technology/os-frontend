222fe417d2fbd16f0886275dfd189c8988a9b4ac
Frooszy <thecryzin@gmail.com>
2024-09-02 21:14:30 -0300

    Scroll visual changes

--- src/contexts/theme/themes.ts
+++ src/contexts/theme/themes.ts
@@ -4,8 +4,24 @@ const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#7A4BFD"
-    }
+      main: "#7A4BFD",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "::-webkit-scrollbar": {
+          width: "10px",
+        },
+        "::-webkit-scrollbar-track": {
+          backgroundColor: "transparent",
+        },
+        "::-webkit-scrollbar-thumb": {
+          backgroundColor: "#888",
+          borderRadius: "4px",
+        },
+      },
+    },
   },
 });
 
@@ -13,23 +29,36 @@ const darkTheme = createTheme({
   palette: {
     mode: "dark",
     primary: {
-      main: "#7A4BFD"
-    }
+      main: "#7A4BFD",
+    },
   },
   components: {
     MuiTextField: {
       styleOverrides: {
         root: {
-          '& input:-webkit-autofill': {
-            backgroundColor: '#f3e5f5',
-            WebkitTextFillColor: 'inherit',
-            WebkitBoxShadow: '0 0 0 100px inherit inset',
+          "& input:-webkit-autofill": {
+            backgroundColor: "#f3e5f5",
+            WebkitTextFillColor: "inherit",
+            WebkitBoxShadow: "0 0 0 100px inherit inset",
           },
         },
       },
     },
+    MuiCssBaseline: {
+      styleOverrides: {
+        "::-webkit-scrollbar": {
+          width: "10px",
+        },
+        "::-webkit-scrollbar-track": {
+          backgroundColor: "transparent",
+        },
+        "::-webkit-scrollbar-thumb": {
+          backgroundColor: "#555",
+          borderRadius: "4px"
+        },
+      },
+    },
   },
-
 });
 
 export { lightTheme, darkTheme };
