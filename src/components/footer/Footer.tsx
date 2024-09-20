976651cc85158f5cc4b4ea3a51f0e15ec4ea550a
João Vitor <thecryzin@gmail.com>
2024-09-20 13:25:59 -0300

    Footer fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className='flex flex-col items-center justify-center pb-2 gap-1'>
-            <span className="text-xs md:text-sm">Copyright © 2024 <a href="https://sambacode.com.br" className="transition hover:text-[#7A4BFD]">Samba Code</a> – Todos os direitos reservados.</span>
+            <span className="text-xs md:text-sm">Copyright © 2024 <a href="https://sambacode.com.br" target="_blank" className="transition hover:text-[#7A4BFD]">Samba Code</a> – Todos os direitos reservados.</span>
         </div>
     )
 }
